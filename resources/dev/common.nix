65ac99ddb7518416a284a39d4921b24088f59153
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-14 12:45:40 +0100

    Add `RUST_SRC_PATH` env variable.

--- resources/dev/common.nix
+++ resources/dev/common.nix
@@ -7,8 +7,11 @@
 let
   mkShellNoCC = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; };
   rust = (pkgs.rust-bin.${version}.latest.${profile}.override { extensions = [ "rust-src" ]; });
+  path = pkgs.rust.packages.${version}.rustPlatform.rustLibSrc;
 in mkShellNoCC {
   name = "rust-" + version + "-" + profile;
+
+  RUST_SRC_PATH = path;
 
   buildInputs = [
     pkgs.openssl
